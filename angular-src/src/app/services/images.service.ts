db1f1dea0856c7d62490c06d5a667b9dd77960e7
David Bautista <dj.bautista10@uniandes.edu.co>
2018-10-25 11:57:11 -0500

    redetek

--- angular-src/src/app/services/images.service.ts
+++ angular-src/src/app/services/images.service.ts
@@ -11,14 +11,14 @@ export class ImagesService {
   getImages() {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('http://192.168.10.113:3000/users/images', { headers: headers }).pipe(
+    return this.http.get('http://192.168.1.54:3000/users/images', { headers: headers }).pipe(
       map(res => res.json()));
   }
 
   eliminarImagen(id) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.delete('http://192.168.10.113:3000/users/removeImage?idImage=' + id, { headers: headers }).pipe(
+    return this.http.delete('http://192.168.1.54:3000/users/removeImage?idImage=' + id, { headers: headers }).pipe(
       map(res => res.json())
     );
   }
